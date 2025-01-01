f51742d563df39c8976e21f5c63577f6a9de0f2d
Vihananand <vihananand2018@gmail.com>
2025-01-01 22:45:38 +0530

    added string concatination

---
+++ LearnJS/stringConcat.js
@@ -0,0 +1,7 @@
+const firstName = "Vihan";
+const lastName = "Anand";
+console.log(firstName + " " + lastName); //Concatenate two strings togather
+
+let nega = "Lebron";
+nega += " James";
+console.log(nega); //Concatenate two string storing it in same variable
