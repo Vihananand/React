5de2dbaf384d48aaaf547ca5f9c1e3458dc6b911
Vihananand <vihananand2018@gmail.com>
2025-01-02 22:35:19 +0530

    updated numbers.js and array.js

--- LearnJS/array.js
+++ LearnJS/array.js
@@ -1,4 +1,5 @@
 const array = [1, 2, 3, 4, 5]; //const doesn't let array to prevent from changing the elements inside it it only prevent the vatriable to change from array type to any other type but still the number of elements inside the array can be manipulated  
-array[5] = 6;
+array.push(6);
 console.log(array);
 console.log(array.length);
+console.log(array.at(-1));
--- LearnJS/numbers.js
+++ LearnJS/numbers.js
@@ -5,6 +5,7 @@ let num = 6969;
 console.log(typeof(num.toString())); //toString() converts a number to string.
 
 console.log("abc" * 55); //This will print NaN(Not a Number). NaN is also considered as a bug.
+console.log(isNaN("lol")); // Checks wether what written inside isNan() function is a number or not if not a number returns true otherwise false
 
 let n = "42";
 console.log(Number.parseInt(n, 10)); //Number.parseInt(string, radix) radix is the base of the number you want to convert into like 10 is for decimal number, 16 is for hexadecimal but it is not convert a number into binary.
