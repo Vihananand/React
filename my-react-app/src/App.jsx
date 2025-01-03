c0c147606a3f5251ead9a85792366d76a53c1d91
Vihananand <vihananand2018@gmail.com>
2025-01-03 21:55:21 +0530

    updated app.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let name = "Vihan Anand";
+  let l = [10, 20, 30, 40];
+  let obj = {
+    'name' : 'Vihan Anand',
+    'company' : "Microsodft",
+    'age' : 20
+  };
+  let age = 20;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <h1>{name}</h1>
+        {
+          l.map((v)=>{
+            return (<div>{v}</div>)
+          })
+        }
+        <div>{obj.company}</div>
+        {age > 18 ? <div>Can vote!!</div> : <div>Cannot vote!!</div>}
       </div>
-      <h1>Vite + ReactJS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
